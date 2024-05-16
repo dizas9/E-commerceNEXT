bd41e02b6eb9e29449a6a64ef6f3d0069b8b573d
dizas9 <sazid.ice07@gmail.com>
2024-05-16 15:57:24 +0600

    Fix responsivness

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex">
       <Product />
-      <div className="w-[40%] m-0 pt-20">
+      <div className="w-[40%] m-0 pt-20 hidden lg:flex">
         <Cart />
       </div>
     </div>
